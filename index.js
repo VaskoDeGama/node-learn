87d9e191b1136c845704e015a5e76affccc457e6
VaskoDeGama <vasr161095@gmail.com>
2020-07-31 21:38:24 +0800

    base express server

--- index.js
+++ index.js
@@ -1,59 +1,8 @@
-const http = require('http')
-const fs = require('fs')
-const path = require('path')
-const PORT = 3000
+const express = require('express')
 
-const server = http.createServer((req, res) => {
-  if (req.method === 'GET') {
-    if (req.url === '/') {
-      res.writeHead(200, { 'Content-Type': 'text/html', charset: 'utf-8' })
-      fs.readFile(
-        path.join(__dirname, 'views', 'index.html'),
-        'utf-8',
-        (err, data) => {
-          if (err) throw err
-          res.write(data)
-          res.end()
-        }
-      )
-    } else if (req.url === '/about') {
-      res.writeHead(200, { 'Content-Type': 'text/html', charset: 'utf-8' })
-      fs.readFile(
-        path.join(__dirname, 'views', 'about.html'),
-        'utf-8',
-        (err, data) => {
-          if (err) throw err
-          res.write(data)
-          res.end()
-        }
-      )
-    } else if (req.url === '/api/users') {
-      res.writeHead(200, { 'Content-Type': 'text/json', charset: 'utf-8' })
-      const users = [
-        {
-          name: 'Vaska',
-          age: 25,
-        },
-        {
-          name: 'Valisiy',
-          age: 21,
-        },
-      ]
-      res.write(JSON.stringify(users))
-      res.end()
-    }
-  } else if (req.method === 'POST') {
-    const body = []
-    req.on('data', (data) => {
-      body.push(Buffer.from(data))
-    })
-    req.on('end', () => {
-      const title = body.toString().split('=')[1]
-      res.end(`Title: ${title}`)
-    })
-  }
-})
+const app = express()
 
-server.listen(PORT, () => {
-  console.log('Server started on port:', PORT)
+const PORT = process.env['PORT'] || 3000
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
 })
