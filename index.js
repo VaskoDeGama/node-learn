d64ee1eb35e4741c1b7032f95fcce419d6ac73d2
VaskoDeGama <vasr161095@gmail.com>
2020-07-31 21:04:25 +0800

    read content from file

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
 const http = require('http')
+const fs = require('fs')
+const path = require('path')
 const PORT = 3000
+
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html', charset: 'utf-8' })
   if (req.method === 'GET') {
-    res.end(`
-            <h1>Form</h1>
-            <form method="post" action="/">
-              <input type="text" name="title" placeholder="title">
-              <button type="submit">Submit</button>
-            </form>
-    `)
+    res.writeHead(200, { 'Content-Type': 'text/html', charset: 'utf-8' })
+    if (req.url === '/') {
+      fs.readFile(
+        path.join(__dirname, 'views', 'index.html'),
+        'utf-8',
+        (err, data) => {
+          if (err) throw err
+          res.write(data)
+          res.end()
+        }
+      )
+    } else if (req.url === '/about') {
+      fs.readFile(
+        path.join(__dirname, 'views', 'about.html'),
+        'utf-8',
+        (err, data) => {
+          if (err) throw err
+          res.write(data)
+          res.end()
+        }
+      )
+    }
   } else if (req.method === 'POST') {
     const body = []
     req.on('data', (data) => {
