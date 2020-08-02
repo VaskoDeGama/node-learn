6cd47d9d3971a95477a17c45c2c85570f75f1dd5
VaskoDeGama <vasr161095@gmail.com>
2020-08-02 21:11:47 +0800

    added card model

--- routes/card.js
+++ routes/card.js
@@ -7,13 +7,16 @@ const router = Router()
 router.post('/add', async (req, res) => {
   const course = await Course.getById(req.body.id)
   await Card.add(course)
-  res.redirect('card')
+  res.redirect('/card')
 })
 
 router.get('/', async (req, res) => {
-  //const card = await Card.fetch()
+  const card = await Card.fetch()
   res.render('card', {
+    isCard: true,
     title: 'Card',
+    courses: card.courses,
+    price: card.price,
   })
 })
 
