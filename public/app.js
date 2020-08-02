94236929f944e929342ae3614b7477f22edf3d1e
VaskoDeGama <vasr161095@gmail.com>
2020-08-02 21:56:44 +0800

    updated remove function from card

--- public/app.js
+++ public/app.js
@@ -1,8 +1,12 @@
-document.querySelectorAll('.price').forEach((node) => {
-  node.textContent = new Intl.NumberFormat('ru-RU', {
+const toFormat = (price) => {
+  return new Intl.NumberFormat('ru-RU', {
     currency: 'rub',
     style: 'currency',
-  }).format(node.textContent)
+  }).format(price)
+}
+
+document.querySelectorAll('.price').forEach((node) => {
+  node.textContent = toFormat(node.textContent)
 })
 
 const $card = document.querySelector('#card')
@@ -14,7 +18,25 @@ if ($card) {
         method: 'delete',
       })
         .then((res) => res.json())
-        .then((card) => console.log(card))
+        .then((card) => {
+          if (card.courses.length) {
+            $card.querySelector('tbody').innerHTML = card.courses
+              .map((c) => {
+                return `
+                          <tr>
+                    <td>${c.title}</td>
+                    <td>${c.count}</td>
+                    <td>
+                      <button class="btn btn-  small js-remove" data-id="${c.id}">Remove</button>
+                    </td>
+                  </tr>`
+              })
+              .join('')
+            $card.querySelector('.price').textContent = toFormat(card.price)
+          } else {
+            $card.innerHTML = '<p>Card is empty</p>'
+          }
+        })
     }
   })
 }
